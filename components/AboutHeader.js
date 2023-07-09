45a42f86a89568035892ae830caaff872026230f
Kamil Pawlak <hello@kamilpawlak.com>
2023-07-09 18:59:14 +0200

    feat: move to Contentful for resume hosting

--- components/AboutHeader.js
+++ components/AboutHeader.js
@@ -1,40 +1,75 @@
 export default function AboutHeader(props) {
   const checkRelocation = (value) => {
     if (value == false) {
-      return <span className='about-header-infobox-noremote'> • Remote or local work only, not available for relocation</span>;
+      return (
+        <span className="about-header-infobox-noremote">
+          {" "}
+          • Remote or local work only, not available for relocation
+        </span>
+      );
     }
-  }
+  };
   return (
-    <header className='about-header'>
-      <div className='about-header-container'>
-        <div className='about-header-left'>
-          <img src='/static/face.jpg' className='about-header-photo'
-          alt='My face'/>
+    <header className="about-header">
+      <div className="about-header-container">
+        <div className="about-header-left">
+          <img
+            src="/static/face.jpg"
+            className="about-header-photo"
+            alt="My face"
+          />
         </div>
-        <div className='about-header-right'>
+        <div className="about-header-right">
           <h1>Kamil Pawlak</h1>
           <h3>Front-end web developer</h3>
-          <div className='about-header-infobox'>
-            <div className='about-header-infobox-item'>
-              <img src='/static/icons/place.svg' className='about-header-infobox-icon' alt='Location'/>
-              <p>{props.location[0].location}{checkRelocation(props.location[0].availableForRelocation)}</p>
+          <div className="about-header-infobox">
+            <div className="about-header-infobox-item">
+              <img
+                src="/static/icons/place.svg"
+                className="about-header-infobox-icon"
+                alt="Location"
+              />
+              <p>
+                {props.location[0].location}
+                {checkRelocation(props.location[0].availableForRelocation)}
+              </p>
             </div>
-            <div className='about-header-infobox-item'>
+            <div className="about-header-infobox-item">
               {props.social.map((social, index) => (
-                <a href={social.link} className='about-header-infobox-link' key={index} target="_blank" rel="noopener noreferrer">
-                  <img src={`/static/icons/${social.icon}.svg`} alt={social.name}/>
+                <a
+                  href={social.link}
+                  className="about-header-infobox-link"
+                  key={index}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={`/static/icons/${social.icon}.svg`}
+                    alt={social.name}
+                  />
                   {social.name}
                 </a>
               ))}
-              <a className='about-header-infobox-link' href={`mailto:${props.emails[0].email}`}>
-                <img src={`/static/icons/email.svg`} alt='Email'/>
+              <a
+                className="about-header-infobox-link"
+                href={`mailto:${props.emails[0].email}`}
+              >
+                <img src={`/static/icons/email.svg`} alt="Email" />
                 Email
               </a>
             </div>
             {props.resumeLinks ? (
-              <div className='about-header-infobox-item'>
-                <a className='about-header-infobox-link' href={props.resumeLinks[0].url} target="_blank" rel="noopener noreferrer">
-                  <img src='/static/icons/account-details-white.svg' alt='Resume'/>
+              <div className="about-header-infobox-item">
+                <a
+                  className="about-header-infobox-link"
+                  href={props.resumeLinks[0].file.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="/static/icons/account-details-white.svg"
+                    alt="Resume"
+                  />
                   My CV/resume
                 </a>
               </div>
@@ -43,5 +78,5 @@ export default function AboutHeader(props) {
         </div>
       </div>
     </header>
-  )
+  );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,178 +1,212 @@
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import AboutHeader from '../components/AboutHeader'
-import AboutBio from '../components/AboutBio'
-import AboutInfoContainer from '../components/AboutInfoContainer'
-import AboutEducationCard from '../components/AboutEducationCard'
-import AboutExperienceCard from '../components/AboutExperienceCard'
-import AboutTechnologyGallery from '../components/AboutTechnologyGallery'
-import ContactFooter from '../components/ContactFooter'
-import BottomNavBar from '../components/BottomNavBar'
-import { gql } from '@apollo/client'
-import { initializeApollo } from '../lib/apolloClient'
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import AboutHeader from "../components/AboutHeader";
+import AboutBio from "../components/AboutBio";
+import AboutInfoContainer from "../components/AboutInfoContainer";
+import AboutEducationCard from "../components/AboutEducationCard";
+import AboutExperienceCard from "../components/AboutExperienceCard";
+import AboutTechnologyGallery from "../components/AboutTechnologyGallery";
+import ContactFooter from "../components/ContactFooter";
+import BottomNavBar from "../components/BottomNavBar";
+import { gql } from "@apollo/client";
+import { initializeApollo } from "../lib/apolloClient";
 
 export function About(props) {
   return (
-    <div id='site-root'>
+    <div id="site-root">
       <Head>
         <title>About me • Kamil Pawlak - front-end web developer</title>
         <meta property="og:url" content="https://kamilpawlak.com/about" />
-        <meta property="og:title" content="About me • Kamil Pawlak - front-end web developer" />
+        <meta
+          property="og:title"
+          content="About me • Kamil Pawlak - front-end web developer"
+        />
       </Head>
-      <Navbar navItems={props.menuItems} emails={props.emails} activePage='/about'/>
-      <AboutHeader location={props.aboutLocationInfo} social={props.aboutSocialLinks} emails={props.emails} resumeLinks={props.resumeLinks}/>
+      <Navbar
+        navItems={props.menuItems}
+        emails={props.emails}
+        activePage="/about"
+      />
+      <AboutHeader
+        location={props.aboutLocationInfo}
+        social={props.aboutSocialLinks}
+        emails={props.emails}
+        resumeLinks={props.resumeLinks}
+      />
       <main>
-        <AboutBio text={props.aboutInfoText}/>
+        <AboutBio text={props.aboutInfoText} />
         <AboutInfoContainer>
-          <div className='about-info-grid'>
-            <div className='about-info-grid-left'>
+          <div className="about-info-grid">
+            <div className="about-info-grid-left">
               <h1>Work experience</h1>
             </div>
-            <div className='about-info-grid-right'>
+            <div className="about-info-grid-right">
               {props.aboutExperienceItems.map((item, index) => (
                 <AboutExperienceCard
-                companyName={item.companyName}
-                jobTitle={item.jobTitle}
-                image={item.image}
-                location={item.location}
-                time={item.date}
-                description={item.description}
-                key={index}
+                  companyName={item.companyName}
+                  jobTitle={item.jobTitle}
+                  image={item.image}
+                  location={item.location}
+                  time={item.date}
+                  description={item.description}
+                  key={index}
                 />
               ))}
-                <hr className='about-info-separator'/>
+              <hr className="about-info-separator" />
             </div>
           </div>
-          <div className='about-info-grid'>
-            <div className='about-info-grid-left'>
-              <h1>Technologies<br/>that I use</h1>
+          <div className="about-info-grid">
+            <div className="about-info-grid-left">
+              <h1>
+                Technologies
+                <br />
+                that I use
+              </h1>
             </div>
-            <div className='about-info-grid-right'>
-              <AboutTechnologyGallery images={props.aboutTechnologyItems}/>
-              <hr className='about-info-separator'/>
+            <div className="about-info-grid-right">
+              <AboutTechnologyGallery images={props.aboutTechnologyItems} />
+              <hr className="about-info-separator" />
             </div>
           </div>
-          <div className='about-info-grid'>
-            <div className='about-info-grid-left'>
+          <div className="about-info-grid">
+            <div className="about-info-grid-left">
               <h1>Education</h1>
             </div>
-            <div className='about-info-grid-right'>
+            <div className="about-info-grid-right">
               {props.aboutEducationItems.map((item, index) => (
                 <AboutEducationCard
-                schoolName={item.schoolName}
-                degreeInfo={item.degreeInfo}
-                image={item.image}
-                location={item.location}
-                time={item.time}
-                key={index}
+                  schoolName={item.schoolName}
+                  degreeInfo={item.degreeInfo}
+                  image={item.image}
+                  location={item.location}
+                  time={item.time}
+                  key={index}
                 />
               ))}
             </div>
           </div>
         </AboutInfoContainer>
       </main>
-      <ContactFooter links={props.socialLinks} emails={props.emails}/>
-      <BottomNavBar activePage='/about' emails={props.emails}/>
+      <ContactFooter links={props.socialLinks} emails={props.emails} />
+      <BottomNavBar activePage="/about" emails={props.emails} />
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const apolloClient = initializeApollo()
-  
+  const apolloClient = initializeApollo();
+
   await apolloClient.query({
     query: gql`
-    query {
-      menuItemCollection {
-        items {
-          title
-          link
+      query {
+        menuItemCollection {
+          items {
+            title
+            link
+          }
         }
-      }
-      aboutPageLocationInfoCollection {
-        items {
-          location
-          availableForRelocation
+        aboutPageLocationInfoCollection {
+          items {
+            location
+            availableForRelocation
+          }
         }
-      }
-      aboutPageSocialLinkCollection {
-        items {
-          name
-          icon
-          link
+        aboutPageSocialLinkCollection {
+          items {
+            name
+            icon
+            link
+          }
         }
-      }
-      aboutPageInfoTextCollection {
-        items {
-          text
+        aboutPageInfoTextCollection {
+          items {
+            text
+          }
         }
-      }
-      aboutPageExperienceItemCollection {
-        items {
-          companyName
-          jobTitle
-          location
-          date
-          description
-          image {
+        aboutPageExperienceItemCollection {
+          items {
+            companyName
+            jobTitle
+            location
+            date
+            description
+            image {
+              url
+            }
+          }
+        }
+        aboutPageEducationItemCollection {
+          items {
+            schoolName
+            degreeInfo
+            location
+            time
+            image {
+              url
+            }
+          }
+        }
+        aboutPageTechnologyItemCollection(order: [sys_publishedAt_ASC]) {
+          items {
+            name
+            logo {
+              url
+            }
+          }
+        }
+        socialLinkCollection {
+          items {
+            name
+            text
+            icon
             url
           }
         }
-      }
-      aboutPageEducationItemCollection {
-        items {
-          schoolName
-          degreeInfo
-          location
-          time
-          image {
-            url
+        emailCollection {
+          items {
+            email
+          }
+        }
+        resumeLinkCollection {
+          items {
+            file {
+              url
+            }
           }
         }
       }
-      aboutPageTechnologyItemCollection(order: [sys_publishedAt_ASC]) {
-        items {
-          name
-          logo {
-            url
-          }
-        }
-      }
-      socialLinkCollection {
-        items {
-          name
-          text
-          icon
-          url
-        }
-      }
-      emailCollection {
-        items {
-          email
-        }
-      }
-      resumeLinkCollection {
-        items {
-          url
-        }
-      }
-    }
     `,
-  })
+  });
   return {
     props: {
-      menuItems: apolloClient.cache.extract().ROOT_QUERY.menuItemCollection.items,
+      menuItems:
+        apolloClient.cache.extract().ROOT_QUERY.menuItemCollection.items,
       emails: apolloClient.cache.extract().ROOT_QUERY.emailCollection.items,
-      socialLinks: apolloClient.cache.extract().ROOT_QUERY.socialLinkCollection.items,
-      aboutLocationInfo: apolloClient.cache.extract().ROOT_QUERY.aboutPageLocationInfoCollection.items,
-      aboutSocialLinks: apolloClient.cache.extract().ROOT_QUERY.aboutPageSocialLinkCollection.items,
-      aboutInfoText: apolloClient.cache.extract().ROOT_QUERY.aboutPageInfoTextCollection.items,
-      aboutExperienceItems: apolloClient.cache.extract().ROOT_QUERY.aboutPageExperienceItemCollection.items,
-      aboutEducationItems: apolloClient.cache.extract().ROOT_QUERY.aboutPageEducationItemCollection.items,
-      aboutTechnologyItems: apolloClient.cache.extract().ROOT_QUERY['aboutPageTechnologyItemCollection({"order":["sys_publishedAt_ASC"]})'].items,
-      resumeLinks: apolloClient.cache.extract().ROOT_QUERY.resumeLinkCollection.items
-    }
-  }
+      socialLinks:
+        apolloClient.cache.extract().ROOT_QUERY.socialLinkCollection.items,
+      aboutLocationInfo:
+        apolloClient.cache.extract().ROOT_QUERY.aboutPageLocationInfoCollection
+          .items,
+      aboutSocialLinks:
+        apolloClient.cache.extract().ROOT_QUERY.aboutPageSocialLinkCollection
+          .items,
+      aboutInfoText:
+        apolloClient.cache.extract().ROOT_QUERY.aboutPageInfoTextCollection
+          .items,
+      aboutExperienceItems:
+        apolloClient.cache.extract().ROOT_QUERY
+          .aboutPageExperienceItemCollection.items,
+      aboutEducationItems:
+        apolloClient.cache.extract().ROOT_QUERY.aboutPageEducationItemCollection
+          .items,
+      aboutTechnologyItems:
+        apolloClient.cache.extract().ROOT_QUERY[
+          'aboutPageTechnologyItemCollection({"order":["sys_publishedAt_ASC"]})'
+        ].items,
+      resumeLinks:
+        apolloClient.cache.extract().ROOT_QUERY.resumeLinkCollection.items,
+    },
+  };
 }
 
-export default About
+export default About;
